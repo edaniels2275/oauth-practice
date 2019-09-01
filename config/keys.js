56cabc73f3148435c1980e393554065ce82bb7f6
edaniels2275 <edaniels2275@gmail.com>
2019-09-01 12:19:08 -0400

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+// add this file to .gitignore,
+//which keeps keys secret from people
+//online
+
+module.exports = {
+    google: {
+        clientID: '1077629668732 - j2bqv09ebcqtlndvk6ji24542mqa6p87.apps.googleusercontent.com',
+        clientSecret: 'AzRiLmghAFDy0Hps6HtPpn0I'
+    }
+}
